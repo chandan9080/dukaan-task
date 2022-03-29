5db52c6bcdc84c4f47cfffe7b5243af871da3c29
Chandan <54211830+chandan9080@users.noreply.github.com>
2022-03-29 15:12:06 +0530

    dukaan-task

---
+++ src/assets/data/slogons.js
@@ -0,0 +1,146 @@
+export default slogons = [
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "There is no Sore it will Not Heal, No cool it will not Subdue.",
+  },
+  {
+    id: 2,
+    message: "There is no Sore it will Not Heal, No cool it will not Subdue.",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+  {
+    id: 1,
+    message: "I am a full stack developer",
+  },
+];
